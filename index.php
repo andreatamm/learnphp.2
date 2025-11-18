de11e9dc03a34e34db2370237320af73732cb8df
Andrea Tamm <tamm.andrea2005@gmail.com>
2025-11-18 17:26:13 +0200

    Add variable examples

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo("Hello World!\n");
+echo "Hello World!\n";
+$name = 'Andrea';
+echo 'Hello ' . $name;
+echo "\n";
+$age = 20;
+echo 'Your name is ' . $name . ' and you are ' . $age . ' years old!';
+echo "\n";
+echo 'Your name is $name and you are $age years old!\n';
+echo "\n";
+echo "Your name is $name and you are $age years old!\n";
+
+$var = 1;
+var_dump($var);
+$var = 1.3;
+var_dump($var);
+$var = '💩';
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null;
+var_dump($bar);
+
+?>
