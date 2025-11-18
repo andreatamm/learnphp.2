07f1a24388763da51fbe0b4e808f082aa79022e6
Andrea Tamm <tamm.andrea2005@gmail.com>
2025-11-18 17:37:48 +0200

    Add static example

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ class Box {
     private $width = 0;
     protected $length = 0;
 
+    public static $count = 0;
+
+    public static function cool(){
+        var_dump(self::$count); 
+        var_dump(self::class); 
+        var_dump(static::class); 
+    }
+
     public function __construct($height=0, $width=0, $length=0) {
         $this->height = $height;
         $this->width = $width;
@@ -44,7 +52,16 @@ class IronBox extends MetalBox {
     public $weightPerUnit = 3;
 }
 
-$metalBox = new MetalBox(10, 20, 40);
-var_dump($metalBox);
-var_dump($metalBox->volume());
-var_dump($metalBox->mass());
+
+Box::$count = 1;
+var_dump(Box::$count);
+
+Box::$count = 2;
+var_dump(Box::$count);
+
+var_dump(Box::$count);
+var_dump(MetalBox::$count);
+var_dump(Box::class);
+Box::cool();
+
+MetalBox::cool();
